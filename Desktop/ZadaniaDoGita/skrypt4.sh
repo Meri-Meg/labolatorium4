8b2d12b723c852eca9106009a2551c9e3f157a30
Meri-Meg <karol.filipczyk2002@gmail.com>
2024-05-06 10:31:47 +0200

    4

---
+++ Desktop/ZadaniaDoGita/skrypt4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ "$1" != "--logs30" ]; then
+  echo "Niepoprawna flaga. Użycie: skrypt.sh --logs30"
+  exit 1
+fi
+if [ -z "$2" ]; then
+  echo "Nie podano liczby plików. Użycie: skrypt.sh --logs30 <liczba_plikow>"
+  exit 1
+fi
+num_files=$2
+for ((i=1; i<=$num_files; i++)); do
+  filename="log$i.txt"
+  echo "Nazwa pliku: $filename" > "$filename"
+  echo "Nazwa skryptu: skrypt.sh" >> "$filename"
+  echo "Data utworzenia: $(date)" >> "$filename"
+done
+echo "Utworzono $num_files plików logX.txt."
